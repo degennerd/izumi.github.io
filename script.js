bd8bb544f201c72c86a8085e16600628346ae9ad
degennerd <degen_nerd@icloud.com>
2024-10-24 08:34:24 -0600

    Add files via upload

--- script.js
+++ script.js
@@ -639,3 +639,10 @@ function updateLanguage(lang) {
 
     hideDropdown();
   }
+
+
+  document.querySelector('.dropdown-btn').addEventListener('click', function() {
+    const dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+  });
+  
